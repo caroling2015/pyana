85882b5d62ec3f364e44056a345476fe72aae708
caroling <liuke_7700@163.com>
2019-06-26 14:46:39 +0800

    build db

---
+++ anaMoney/baseSQL.py
@@ -0,0 +1,21 @@
+#-*- coding:UTF-8  -*-
+import sqlite3
+
+def create(conn):
+    sql_create ='''
+    CREATE TABLE 'users'(
+    'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    'username' TEXT NOT NULL UNIQUE,
+    'password' TEXT NOT NULL,
+    'email' TEXT
+    )
+    '''
+    conn.execute(sql_create)
+    print('创建成功')
+
+
+
+
+
+
+ # if '__name__'=='__main__':
---
+++ anaMoney/consqlite.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+def convert(value):
+    if value.startswith('~'):
+        return value.strip('~')
+    if not value:
+        value='0'
+        return float(value)
+
+
+def cTable():
+    con = sqlite3.connect ('food.db')
+    curs = con.cursor ()
+    curs.execute ('''
+        CREATE TABLE 'food'(
+        'id' TEXT PRIMARY KEY,
+        'desc' TEXT,
+        'water' FLOAT,
+        'kcal' FLOAT,
+        'protein' FLOAT,
+        'fat' FLOAT,
+        'ash' FLOAT,
+        'carbs' FLOAT,
+        'fiber' FLOAT,
+        'sugar' FLOAT)
+    ''')
+
+    query = 'INSERT INTO food VALUES (?,?,?,?,?,?,?,?,?,?,)'
+
+    for line in open ('ADD_ABBR.txt'):
+        fields = line.split ('^')
+        vals = [convert (f) for f in fields]
+        curs.execute (query, vals)
+
+    con.commit ()
+    con.close ()
+
+
+if '__name__' == '__main__':
+    cTable()
+
